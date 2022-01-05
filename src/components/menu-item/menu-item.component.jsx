934b77ce9f9b34f76aec3282bc58b088ca16be3d
Chetan <chetangaddam71@gmail.com>
2022-01-05 19:19:43 +0530

    completedHomePage

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -3,10 +3,11 @@ import "./menu-item.styles.scss";
 
 const MenuItem = ({ title, subtitle, imageUrl, size }) => {
   return (
-    <div
-      className={`${size} menu-item`}
-      style={{ backgroundImage: `url(${imageUrl})` }}
-    >
+    <div className={`${size} menu-item`}>
+      <div
+        style={{ backgroundImage: `url(${imageUrl})` }}
+        className="background-image"
+      />
       <div className="content">
         <h1 className="title">{title}</h1>
         <span className="subtitle">{subtitle}</span>
